69d6714df0139db927eb3f91fa593bfb76204390
minji kim <minjii7874@gmail.com>
2026-09-15 14:31:33 +0900

    0915 전봇대

--- 0915/0915 - 전봇대.py
+++ 0915/0915 - 전봇대.py
@@ -2,5 +2,18 @@ T = int(input())
 
 for tc in range(1, T+1):
     N = int(input())
-    A, B = map(int(input.split()))
+    arr = [list(map(int,input().split())) for _ in range(N)]
 
+
+    answer = 0
+
+    for i in range(N):
+        for j in range(i, N):
+            a, b= arr[i]
+            c, d = arr[j]
+
+
+            if (a > c and b <d) or (a <c and b>d):
+                answer +=1
+
+    print(f"#{tc} {answer}")
